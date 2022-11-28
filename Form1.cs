99ab21d3e8812c1fddb8911ab8a88cf097303a5f
KarolK <kkacprzak2115@gmail.com>
2022-11-28 15:36:42 +0100

    Added resources, added board generator

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pacman_KCK.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,10 @@ namespace Pacman_KCK
 {
     public partial class Form1 : Form
     {
+        private static BoardGenerator boardGenerator = new BoardGenerator();
         public Form1()
         {
             InitializeComponent();
         }
-
     }
 }
